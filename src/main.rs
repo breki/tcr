36958c1b2b63129a52a23f64d757ad8e120c7fd4
Igor Brejc <igor.brejc@gmail.com>
2022-02-19 11:31:59 +0100

    Added notify for file watching.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate notify;
+
+use notify::{Watcher, RecursiveMode, watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Create a watcher object, delivering debounced events.
+    // The notification back-end is selected based on the platform.
+    let watch_period = Duration::from_secs(2);
+
+    let mut watcher = watcher(tx, watch_period).unwrap();
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes. 
+    watcher.watch("d:\\src\\tcr", RecursiveMode::Recursive).unwrap();
+
+    loop {
+        match rx.recv() { 
+           Ok(event) => println!("{:?}", event),
+           Err(e) => println!("watch error: {:?}", e),
+        }
+    }
 }
