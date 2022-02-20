3fac65ebe7738c862f12e3843eacd68185ac109a
Igor Brejc <igor.brejc@gmail.com>
2022-02-20 07:25:11 +0100

    Implemented basic regex filtering of watch events.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate notify;
 
-use notify::{Watcher, RecursiveMode, watcher};
+use notify::DebouncedEvent::NoticeWrite;
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use regex::RegexSet;
+use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
 fn main() {
+    let matching_files = RegexSet::new(&[r".*.\.rs$"]).unwrap();
+
     // Create a channel to receive the events.
     let (tx, rx) = channel();
 
@@ -15,13 +20,41 @@ fn main() {
     let mut watcher = watcher(tx, watch_period).unwrap();
 
     // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes. 
-    watcher.watch("d:\\src\\tcr", RecursiveMode::Recursive).unwrap();
+    // below will be monitored for changes.
+    watcher
+        .watch("d:\\src\\tcr", RecursiveMode::Recursive)
+        .unwrap();
 
     loop {
-        match rx.recv() { 
-           Ok(event) => println!("{:?}", event),
-           Err(e) => println!("watch error: {:?}", e),
+        match rx.recv() {
+            Ok(event) => handle_watch_event(event, &matching_files),
+            Err(e) => println!("watch error: {:?}", e),
         }
     }
+}
+
+fn handle_watch_event(event: DebouncedEvent, matching_files: &RegexSet) {
+    let matching_event: Option<String> = match event {
+        NoticeWrite(path) => {
+            if is_path_matched(&path, matching_files) {
+                Some(path_to_str(&path))
+            } else {
+                None
+            }
+        }
+        _ => None,
+    };
+
+    if matching_event.is_some() {
+        println!("{:?} written", matching_event.unwrap())
+    }
+}
+
+fn is_path_matched(path: &PathBuf, matching_files: &RegexSet) -> bool {
+    return matching_files.is_match(&path_to_str(&path));
+}
+
+fn path_to_str(path: &Path) -> String {
+    let os_path_str = path.as_os_str();
+    os_path_str.to_str().unwrap().to_string()
 }
