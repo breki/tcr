1b0c75277681213ac5115f91d27551f0887679c2
Igor Brejc <igor.brejc@gmail.com>
2022-02-25 06:29:29 +0100

    Rewriting the CLI to use the builder syntax.

--- tcr/src/main.rs
+++ tcr/src/main.rs
@@ -1,6 +1,6 @@
 extern crate notify;
 
-use clap::Parser;
+use clap::{arg, Arg, Command as ClapCommand};
 use notify::DebouncedEvent::{Create, Remove, Rename, Write};
 use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use regex::RegexSet;
@@ -11,48 +11,76 @@ use std::process::Command;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
-#[derive(Parser, Debug)]
-#[clap(name = "tcr")]
-#[clap(author = "Igor Brejc")]
-#[clap(version = "1.0")]
-#[clap(about = "Helps run 'test && commit || revert' workflow")]
-struct Args {
-    /// Program or script to run as a test step. Does nothing if not specified.
-    #[clap(short, long)]
-    test: Option<String>,
-
-    /// Path to watch.
-    #[clap(short, long, default_value = ".")]
-    path: String,
-
-    /// Pattern of files to watch.
-    #[clap(short, long, default_value = r".*.\.rs$")]
-    file_pattern: String,
-
-    /// Delay (in milliseconds) before executing the tests.
-    #[clap(short, long, default_value = "1000")]
-    delay: u64,
-}
-
 fn main() {
-    let args = Args::parse();
+    let matches = ClapCommand::new("tcr")
+        .trailing_var_arg(true)
+        .version("1.0")
+        .author("Igor Brejc")
+        .about("Runs 'test && commit || revert' workflow.")
+        .arg(
+            Arg::new("TEST STEP")
+                .long("test")
+                .short('t')
+                .required(false)
+                .help(
+                    "The command to run as a test step. If not specified, \
+                    only a warning will be printed during the test step.",
+                )
+                .display_order(1),
+        )
+        .arg(
+            arg!(-p --path <PATH>)
+                .required(false)
+                .default_value(".")
+                .help("The path to watch for file changes.")
+                .display_order(2),
+        )
+        .arg(
+            Arg::new("FILE PATTERN")
+                .long("file-pattern")
+                .short('f')
+                .required(false)
+                .default_value(".*.rs")
+                .help(
+                    "The regex file pattern which changed/created/deleted \
+                    files must match to trigger the test step.",
+                )
+                .display_order(3),
+        )
+        .arg(
+            arg!(-d --delay <DELAY>)
+                .required(false)
+                .default_value("1000")
+                .help(
+                    "The delay (in milliseconds) between the first detected k\
+                    file change and running the test step.",
+                )
+                .validator(|s| s.parse::<u64>()),
+        )
+        .get_matches();
 
-    let test_step = args.test;
+    let test_step = matches.value_of("test");
+    let file_pattern = matches.value_of("FILE PATTERN").unwrap();
 
-    let matching_files = RegexSet::new(&[args.file_pattern]).unwrap();
+    let matching_files = RegexSet::new(&[file_pattern]).unwrap();
 
     // Create a channel to receive the events.
     let (tx, rx) = channel();
 
     // Create a watcher object, delivering debounced events.
     // The notification back-end is selected based on the platform.
-    let watch_period = Duration::from_millis(args.delay);
+    let delay = matches.value_of("delay").unwrap().parse::<u64>().unwrap();
+    let watch_period = Duration::from_millis(delay);
 
     let mut watcher = watcher(tx, watch_period).unwrap();
 
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
-    watcher.watch(args.path, RecursiveMode::Recursive).unwrap();
+    let path_to_watch = matches.value_of("path").unwrap();
+
+    watcher
+        .watch(path_to_watch, RecursiveMode::Recursive)
+        .unwrap();
 
     loop {
         match rx.recv() {
@@ -68,7 +96,7 @@ fn main() {
 fn handle_watch_event(
     event: &DebouncedEvent,
     matching_files: &RegexSet,
-    test_step: &Option<String>,
+    test_step: &Option<&str>,
 ) {
     let event_data: Option<(String, String)> = match event {
         Create(path) => extract_event_data("create", &path, matching_files),
@@ -82,29 +110,27 @@ fn handle_watch_event(
 
     if event_data.is_some() {
         println!("{:?}", event_data.unwrap());
-        // todo now: redesign the test step as an external command
-        // that needs to be run + additional arguments provided after the '--'
-        // argument
+        // todo now: how to support the '--' argument separator?
         println!("TEST");
 
         match test_step {
-            Some(test_command) => {
-                let output = Command::new(test_command)
-                    .output()
-                    .expect("failed to execute process");
-
-                io::stdout().write_all(&output.stdout).unwrap();
-                io::stderr().write_all(&output.stderr).unwrap();
-
-                // todo now: check the test exit code.
-                // In case of success, run commit.
-                // In case of failure, run reset.
-            }
+            Some(test_command) => run_test(test_command),
             None => {
                 println!("The test step has not been specified, doing nothing.")
             }
         }
     }
+}
+
+fn run_test(test_command: &str) {
+    let output = Command::new(test_command)
+        .output()
+        .expect("failed to execute the test step");
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+    // todo now: check the test exit code.
+    // In case of success, run commit.
+    // In case of failure, run reset.
 }
 
 fn extract_event_data(
